d4bb098a3d189141ce1ca201a6e76cd494dfc4f4
william <jackychj@139.com>
2019-05-08 09:53:50 +0800

    Git crashed because of mistaken copying from other OS.Recovered

---
+++ lists/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from lists.models import Item
+from django.shortcuts import render,redirect
+# Create your views here.
+def home_page(request):
+    if request.method=='POST':
+        new_item_text=request.POST['item_text']
+        Item.objects.create(text=new_item_text)
+        return  redirect('/lists/the-only-list-in-the-world/')
+    return render(request,'home.html')
+def view_list(request):
+    items = Item.objects.all()
+    return render(request, 'list.html', {'items': items})
