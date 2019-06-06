19280dc7f5a52c4960926d6130f69e2feb042be8
William Cheng <william@example.com>
2019-06-06 22:14:07 +0800

    Add one line 'Setupclass(cls)'

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,11 +4,13 @@ from selenium.webdriver.common.keys import Keys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
     @classmethod
     def setUpClass(cls):
         for arg in sys.argv:
             if 'liveserver' in arg:
                 cls.server_url = 'http://' + arg.split('=')[1]
+                super().setUpClass()
                 return
         super().setUpClass()
         cls.server_url = cls.live_server_url
@@ -17,53 +19,57 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def tearDownClass(cls):
         if cls.server_url == cls.live_server_url:
             super().tearDownClass()
-	def setUp(self):
-		self.browser = webdriver.Firefox()
-		self.browser.implicitly_wait(5)
-	def tearDown(self):
-		self.browser.quit()
-	def check_for_row_in_list_table(self,row_text):
-		table=self.browser.find_element_by_id('id_list_table')
-		rows=table.find_elements_by_tag_name('tr')
-		self.assertIn(row_text,[row.text for row in rows])
-	def test_can_start_a_list_and_retrieve_it_later(self):
-		# 伊迪丝听说有一个很酷的在线待办事项应用
-		# 她去看了这个应用的首页
-		self.browser.get(self.server_url)
-		# 她注意到网页的标题和头部都包含“To-Do”这个词
-		self.assertIn('To-Do', self.browser.title)
-		header_text = self.browser.find_elements_by_tag_name('h1')[0].text
-		self.assertIn('To-Do', header_text)
-		# 应用邀请她输入一个待办事项
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item'	)
-		# 她在一个文本框中输入了“Buy peacock feathers”（购买孔雀羽毛）
-		# 伊迪丝的爱好是使用假蝇做鱼饵钓鱼
-		inputbox.send_keys('Buy peacock feathers')
-		# 她按回车键后，被带到了一个新URL
-		# 这个页面的待办事项清单中显示了“1: Buy peacock feathers”
-		inputbox.send_keys(Keys.ENTER)
-		import time
-		time.sleep(5)
-		edith_list_url = self.browser.current_url
-		self.assertRegex(edith_list_url, '/lists/.+')  #
-		self.check_for_row_in_list_table('1: Buy peacock feathers')
-		# 页面中又显示了一个文本框，可以输入其他的待办事项
-		# 她输入了“Use peacock feathers to make a fly”（使用孔雀羽毛做假蝇）
-		# 伊迪丝做事很有条理
-		inputbox=self.browser.find_element_by_id('id_new_item')
-		inputbox.send_keys('Use peacock feathers to make a fly')
-		inputbox.send_keys(Keys.ENTER)
 
-		time.sleep(5)
-		# 页面再次更新，她的清单中显示了这两个待办事项
-		self.check_for_row_in_list_table('1: Buy peacock feathers')
-		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-		# 现在一个叫作弗朗西斯的新用户访问了网站
-		## 我们使用一个新浏览器会话
-		## 确保伊迪丝的信息不会从cookie中泄露出来
-		time.sleep(3)
-		self.browser.quit()
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(5)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def check_for_row_in_list_table(self,row_text):
+        table=self.browser.find_element_by_id('id_list_table')
+        rows=table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text,[row.text for row in rows])
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # 伊迪丝听说有一个很酷的在线待办事项应用
+        # 她去看了这个应用的首页
+        self.browser.get(self.server_url)
+        # 她注意到网页的标题和头部都包含“To-Do”这个词
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_elements_by_tag_name('h1')[0].text
+        self.assertIn('To-Do', header_text)
+        # 应用邀请她输入一个待办事项
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item'	)
+        # 她在一个文本框中输入了“Buy peacock feathers”（购买孔雀羽毛）
+        # 伊迪丝的爱好是使用假蝇做鱼饵钓鱼
+        inputbox.send_keys('Buy peacock feathers')
+        # 她按回车键后，被带到了一个新URL
+        # 这个页面的待办事项清单中显示了“1: Buy peacock feathers”
+        inputbox.send_keys(Keys.ENTER)
+        import time
+        time.sleep(5)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')  #
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        # 页面中又显示了一个文本框，可以输入其他的待办事项
+        # 她输入了“Use peacock feathers to make a fly”（使用孔雀羽毛做假蝇）
+        # 伊迪丝做事很有条理
+        inputbox=self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
+        time.sleep(5)
+        # 页面再次更新，她的清单中显示了这两个待办事项
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        # 现在一个叫作弗朗西斯的新用户访问了网站
+        ## 我们使用一个新浏览器会话
+        ## 确保伊迪丝的信息不会从cookie中泄露出来
+        time.sleep(3)
+        self.browser.quit()
 
         self.browser = webdriver.Firefox()
         # 弗朗西斯访问首页
