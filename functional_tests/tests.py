409fb0ef93ee5815a9f09e67ea4811fa54557598
william <jackychj@139.com>
2019-06-06 11:56:40 +0800

    Erased some bugs

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -18,7 +18,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 			super().tearDownClass()
 
 	def setUp(self):
-		self.browser = webdriver.Chrome()
+		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(5)
 	def tearDown(self):
 		self.browser.quit()
@@ -44,7 +44,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		# 这个页面的待办事项清单中显示了“1: Buy peacock feathers”
 		inputbox.send_keys(Keys.ENTER)
 		import time
-		#time.sleep(10)
+		time.sleep(5)
 		edith_list_url = self.browser.current_url
 		self.assertRegex(edith_list_url, '/lists/.+')  #
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
@@ -55,7 +55,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
 
-
+		time.sleep(5)
 		# 页面再次更新，她的清单中显示了这两个待办事项
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
@@ -66,7 +66,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		self.browser.quit()
 
 
-		self.browser = webdriver.Chrome()
+		self.browser = webdriver.Firefox()
 		# 弗朗西斯访问首页
 		# 页面中看不到伊迪丝的清单
 		self.browser.get(self.server_url)
@@ -78,7 +78,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Buy milk')
 		inputbox.send_keys(Keys.ENTER)
-
+		time.sleep(5)
 		# 弗朗西斯获得了他的唯一URL
 		francis_list_url = self.browser.current_url
 		self.assertRegex(francis_list_url, '/lists/.+')
