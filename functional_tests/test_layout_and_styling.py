77ecc4062c5f11dd9bff74c286a3eb3c46a378c8
William Cheng <william@example.com>
2019-06-11 22:01:30 +0800

    moved Fts into their own individual files

---
+++ functional_tests/test_layout_and_styling.py
@@ -0,0 +1,9 @@
+from .base import FunctionalTest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+class LayoutAndStylingTest(FunctionalTest):
+
+    def test_layout_and_styling(self):
+        pass
