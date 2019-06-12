7e40b1ccebc1c0fed68432c3959971aa08c33f63
william <jackychj@139.com>
2019-06-12 19:24:48 +0800

    enforce model validation in list view

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,4 +1,5 @@
 from .base import FunctionalTest
+from selenium.webdriver.common.keys import Keys
 import time
 
 class ItemValidationTest(FunctionalTest):
@@ -7,22 +8,32 @@ class ItemValidationTest(FunctionalTest):
         # 伊迪丝访问首页,不小心提交了一个空待办事项
         # 输入框中没输入内容,她就按下了回车键
         self.browser.get(self.server_url)
-        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
         # 首页刷新了,显示一个错误消息
         # 提示待办事项不能为空
+
         time.sleep(3)
-        error = self.browser.find_element_by_css_selector('.has-error')
+
+        error = self.browser.find_element_by_id('errormsg')
         self.assertEqual(error.text,"You can't have an empty list item")
         # 她输入一些文字,然后再次提交,这次没问题了
-        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
         self.check_for_row_in_list_table('1: Buy milk')
         # 她有点儿调皮,又提交了一个空待办事项
-        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+        inputbox=self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
         # 在清单页面她看到了一个类似的错误消息
-        self.check_for_row_in_list_table('1: Buy milk')
-        error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.text,"You can't have an empty list item")
+        error = self.browser.find_element_by_id('errormsg')
+        self.assertEqual(error.text, "You can't have an empty list item")
+
+
         # 输入文字之后就没问题了
-        self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Make tea')
+        inputbox.send_keys(Keys.ENTER)
+
+        time.sleep(3)
+
         self.check_for_row_in_list_table('1: Buy milk')
         self.check_for_row_in_list_table('2: Make tea')
--- lists/views.py
+++ lists/views.py
@@ -11,10 +11,19 @@ def home_page(request):
 
 def view_list(request,list_id):
     list_= List.objects.get(id=list_id)
+    error = None
+
     if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'], list=list_)
-        return redirect('/lists/%i/' % (list_.id,))
-    return render(request, 'list.html', {'list':list_})
+        try:
+            item = Item.objects.create(text=request.POST['item_text'], list=list_)
+            item.full_clean()
+            item.save()
+            return redirect('/lists/%i/' % (list_.id,))
+        except ValidationError:
+            item.delete()
+            error = "You can't have an empty list item"
+
+    return render(request, 'list.html', {'list':list_, 'error': error})
 
 
 def new_list(request):
@@ -27,10 +36,4 @@ def new_list(request):
         list_.delete()
         error = "You can't have an empty list item"
         return render(request, 'home.html', {"error": error})
-    return redirect('/lists/%i/' % (list_.id,))
-
-
-def add_item(request,list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'],list=list_)
     return redirect('/lists/%i/' % (list_.id,))
