3fd8ca492ffb9b393550e785e3c562c51bd1b551
william <jackychj@139.com>
2019-06-19 13:58:06 +0800

    used form.save to restructure the unit tests

--- lists/forms.py
+++ lists/forms.py
@@ -1,11 +1,14 @@
 from django import forms
-from lists.models import Item
+from lists.models import Item, List
 
 
 EMPTY_LIST_ERROR = "You can't have an empty list item"
 
 
 class ItemForm(forms.models.ModelForm):
+    def save(self, for_list):
+        self.instance.list = for_list
+        return  super().save()
     class Meta:
         model = Item
         fields = ('text',)
