6eedafd339d9ba5384f0934a714a356c634536f6
william <jackychj@139.com>
2019-06-28 13:08:35 +0800

    tried again, failed either.

--- static/tests/list.js
+++ static/tests/list.js
@@ -1,5 +1,5 @@
 $("input").on("keypress", function() {
-        if (document.getElementById("errormsg") && $("input[name='text']").value != '') {
+        if (document.getElementById("errormsg") && $("input").value != '') {
             $("#errormsg").hide();
         }
 });
